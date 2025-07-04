1037ca95267ae5dd0627c7a9918eab4023ef4058
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-07-04 08:35:59 +0700

    Create Cây nhị phân hoàn hảo.cpp

---
+++ Cây nhị phân hoàn hảo.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+struct node {
+	int val;
+	node* left;
+	node* right;
+	node(int x) {
+		val = x;
+		left = right = NULL;
+	}
+};
+void makeRoot(node* root, int u, int v, char c) {
+	if (c == 'L') root->left = new node(v);
+	else root->right = new node(v);
+}
+void insertRoot(node* root, int u, int v, char c) {
+	if (root == NULL)return;
+	if (root->val == u) {
+		makeRoot(root, u, v, c);
+	}
+	else {
+		insertRoot(root->left, u, v, c);
+		insertRoot(root->right, u, v, c);
+	}
+}
+
+int height = 0;
+bool heightCheck(node* root, int h) {
+	if (root == NULL) return true;
+	if (root->left == NULL && root->right == NULL) {
+		if (height == 0) {
+			height = h;
+		}
+		else if (h != height) {
+			return false;
+		}
+	}
+	return heightCheck(root->left, h + 1) && 
+		heightCheck(root->right, h + 1);
+}
+bool checkRoot(node* root) {
+	if (root == NULL) return true;
+	if (root->left == NULL && root->right == NULL) return true;
+	if (root->left == NULL && root->right != NULL) return false;
+	if (root->right == NULL && root->left != NULL) return false;
+	return checkRoot(root->left) && checkRoot(root->right);
+}
+int main()
+{
+	node* root = NULL;
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x, y; char c;
+		cin >> x >> y >> c;
+		if (root == NULL) {
+			root = new node(x);
+			makeRoot(root, x, y, c);
+		}
+		else {
+			insertRoot(root, x, y, c);
+		}
+	}
+	if (heightCheck(root, 0) && checkRoot(root)) {
+		cout << "YES";
+	}
+	else cout << "NO";
+}
