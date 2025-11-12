2cdf2ea5d34e243b5491957eacd7320102416b33
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-11-12 09:58:26 +0700

    Update Thao tác cây AVL.cpp

--- Thao tác cây AVL.cpp
+++ Thao tác cây AVL.cpp
@@ -17,17 +17,16 @@ using namespace std;
 
 struct node {
 	int data;
-	int height;
 	node* left;
 	node* right;
+	int height;
 };
 
-node* makeNode(int x) {
+node* makeRoot(int x) {
 	node* newNode = new node;
 	newNode->data = x;
+	newNode->left = newNode->right = NULL;
 	newNode->height = 1;
-	newNode->left = NULL;
-	newNode->right = NULL;
 	return newNode;
 }
 
@@ -41,10 +40,22 @@ int getBalance(node* root) {
 	return treeHeight(root->left) - treeHeight(root->right);
 }
 
+node* rightRotation(node* y) {
+	node* x = y->left;
+	node* T2 = x->right;
+	//Rotation
+	x->right = y;
+	y->left = T2;
+
+	x->height = max(treeHeight(x->left), treeHeight(x->right)) + 1;
+	y->height = max(treeHeight(y->left), treeHeight(y->right)) + 1;
+	return x;
+}
+
 node* leftRotation(node* x) {
 	node* y = x->right;
 	node* T2 = y->left;
-
+	//Rotation
 	y->left = x;
 	x->right = T2;
 
@@ -53,21 +64,10 @@ node* leftRotation(node* x) {
 	return y;
 }
 
-node* rightRotation(node* y) {
-	node* x = y->left;
-	node* T2 = x->right;
-	x->right = y;
-	y->left = T2;
-
-	x->height = max(treeHeight(x->left), treeHeight(x->right)) + 1;
-	y->height = max(treeHeight(y->left), treeHeight(y->right)) + 1;
-
-	return x;
-
-}
-
-node* insertRoot(node* root, int x) {
-	if (root == NULL) return makeNode(x);
+node* insertRoot(node* root,int x) {
+	if (root == NULL) {
+		return makeRoot(x);
+	}
 	if (x < root->data) {
 		root->left = insertRoot(root->left, x);
 	}
@@ -77,21 +77,43 @@ node* insertRoot(node* root, int x) {
 	else return root;
 	root->height = max(treeHeight(root->left), treeHeight(root->right)) + 1;
 	int balance = getBalance(root);
+	//Rotation
 	//left left
+	/*		30 <-- root
+		   /   
+		  20  <-- root->left->data
+         /
+	    10  <-- x                         */
 	if (balance > 1 && x < root->left->data) {
 		return rightRotation(root);
 	}
 	//right right
+	/*		30 <-- root
+			  \
+			   20 <-- root->right->data
+				 \
+				  10 <-- x     */
 	if (balance < -1 && x > root->right->data) {
 		return leftRotation(root);
 	}
+
 	//left right
+	/*		30  <-- root
+		   /
+		  20  <-- root->left->data
+			\
+			 25   <-- x      */
 	if (balance > 1 && x > root->left->data) {
 		root->left = leftRotation(root->left);
 		return rightRotation(root);
 	}
 	//right left
-	if (balance < -1 && x < root->right->data) {
+	/*		30  <-- root
+			  \
+			   40  <-- root->right->data
+			  /
+		     35  <-- x    */
+	if (balance < -1 && x > root->right->data) {
 		root->right = rightRotation(root->right);
 		return leftRotation(root);
 	}
@@ -106,7 +128,7 @@ node* minValueNode(node* root) {
 	return tmp;
 }
 
-node* deleteNode(node* root, int x) {
+node* deleteNode(node* root,int x) {
 	if (root == NULL) return root;
 	if (x < root->data) {
 		root->left = deleteNode(root->left, x);
@@ -131,23 +153,45 @@ node* deleteNode(node* root, int x) {
 			root->right = deleteNode(root->right, tmp->data);
 		}
 	}
-	root->height = max(treeHeight(root->left), treeHeight(root->right)) + 1;
 	int balance = getBalance(root);
-
+	root->height = max(treeHeight(root->left), treeHeight(root->right)) + 1;
+	//Rotation
+	//x la node them, root la dinh cua cay
 	//left left
+	/*		30 <-- root
+		   /
+		  20  <-- root->left->data
+		 /
+		10  <-- x                         */
 	if (balance > 1 && getBalance(root->left) >= 0) {
 		return rightRotation(root);
 	}
 	//right right
+	/*		30 <-- root
+			  \
+			   20 <-- root->right->data
+				 \
+				  10 <-- x     */
 	if (balance < -1 && getBalance(root->right) <= 0) {
 		return leftRotation(root);
 	}
+
 	//left right
+	/*		30  <-- root
+		   /
+		  20  <-- root->left->data
+			\
+			 25   <-- x      */
 	if (balance > 1 && getBalance(root->left) < 0) {
 		root->left = leftRotation(root->left);
 		return rightRotation(root);
 	}
 	//right left
+	/*		30  <-- root
+			  \
+			   40  <-- root->right->data
+			  /
+			 35  <-- x    */
 	if (balance < -1 && getBalance(root->right) > 0) {
 		root->right = rightRotation(root->right);
 		return leftRotation(root);
@@ -156,7 +200,7 @@ node* deleteNode(node* root, int x) {
 }
 
 void inOrder(node* root) {
-	if (root == NULL) return;
+	if (root == NULL) return;	
 	inOrder(root->left);
 	cout << root->data << " ";
 	inOrder(root->right);
